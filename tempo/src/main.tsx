a474cf260ada1d86981035c0b91f5efcc8e18709
swathysukumaran <swathysukumaran@gmail.com>
2025-01-16 15:24:47 -0800

    Add create trip route

--- tempo/src/main.tsx
+++ tempo/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateTrip from "./create-trip/index.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/create-trip",
+    element: <CreateTrip />,
   },
 ]);
 createRoot(document.getElementById("root")!).render(
