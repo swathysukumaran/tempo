102c073810b783da58bb26e0d56d48d11c44d7f0
swathysukumaran <swathysukumaran@gmail.com>
2025-01-17 11:54:12 -0800

    Add hero caption

--- tempo/src/components/custom/Hero.tsx
+++ tempo/src/components/custom/Hero.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="flex items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[50px] text-center mt-16">
+        <span className="text-[#f56551]">
+          Discover Your Next Adventure with AI:
+        </span>
+        Personalized Itineraries at Your Fingertips
+      </h1>
+    </div>
+  );
 }
 
 export default Hero;
