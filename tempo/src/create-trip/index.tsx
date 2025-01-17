834013fe849d2ef46a44d71536040cc580caae26
swathysukumaran <swathysukumaran@gmail.com>
2025-01-17 15:05:43 -0800

    Add destination search

--- tempo/src/create-trip/index.tsx
+++ tempo/src/create-trip/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 
 function CreateTrip() {
-  return <div></div>;
+  return (
+    <div className="sm:px-10 md:px-32 lg:px-56xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preferences.
+      </p>
+
+      <div className="mt-20">
+        <div>
+          <h2>What is destination of choice?</h2>
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTrip;
