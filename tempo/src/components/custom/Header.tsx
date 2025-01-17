1a1326bd0075e2b277ff0c918c5966c18f4f1ed8
swathysukumaran <swathysukumaran@gmail.com>
2025-01-17 11:31:36 -0800

    Add button to header

--- tempo/src/components/custom/Header.tsx
+++ tempo/src/components/custom/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "../ui/button";
 
 function Header() {
   return (
-    <div className="p-2 shadow-sm">
+    <div className="p-2 shadow-sm flex justify-between items-center ">
       <img src="/logo.svg" alt="logo" />
+      <div>
+        <Button>Sign in</Button>
+      </div>
     </div>
   );
 }
