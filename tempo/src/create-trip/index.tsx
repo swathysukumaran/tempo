7a7fba9cb098bd39c56747d6c64c92fa92afc086
swathysukumaran <swathysukumaran@gmail.com>
2025-01-19 14:41:52 -0800

    Display budget options

--- tempo/src/create-trip/index.tsx
+++ tempo/src/create-trip/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { Input } from "../components/ui/input";
+import { SelectBudgetOptions } from "@/constants/options";
 type Option = {
   label: string;
   value: string;
@@ -47,6 +48,22 @@ function CreateTrip() {
             How many days are you planning your trip?
           </h2>
           <Input placeholder={"EX.3"} type="number" />
+        </div>
+
+        <div>
+          <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectBudgetOptions.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border curosor-pointer rounded-lg hover:shadow-lg"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
