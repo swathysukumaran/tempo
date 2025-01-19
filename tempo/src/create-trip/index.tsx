640de6d00d365ebbc4b37ea415e77ff87e91e8cb
swathysukumaran <swathysukumaran@gmail.com>
2025-01-19 14:46:21 -0800

    Display travel options

--- tempo/src/create-trip/index.tsx
+++ tempo/src/create-trip/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { Input } from "../components/ui/input";
-import { SelectBudgetOptions } from "@/constants/options";
+import { SelectBudgetOptions, SelectTravelersList } from "@/constants/options";
 type Option = {
   label: string;
   value: string;
@@ -54,6 +54,24 @@ function CreateTrip() {
           <h2 className="text-xl my-3 font-medium">What is Your Budget?</h2>
           <div className="grid grid-cols-3 gap-5 mt-5">
             {SelectBudgetOptions.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border curosor-pointer rounded-lg hover:shadow-lg"
+              >
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2 className="text-xl my-3 font-medium">
+            Who do you plan on traveling with on your next adventure?
+          </h2>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectTravelersList.map((item, index) => (
               <div
                 key={index}
                 className="p-4 border curosor-pointer rounded-lg hover:shadow-lg"
