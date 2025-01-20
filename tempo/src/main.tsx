c1fc3bc29f2efad97456eef56043e1ce8d57645f
swathysukumaran <swathysukumaran@gmail.com>
2025-01-20 01:02:38 -0800

    Add google authentication

--- tempo/src/main.tsx
+++ tempo/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import CreateTrip from "./create-trip/index.tsx";
 import Header from "./components/custom/Header.tsx";
 import { Toaster } from "./components/ui/sonner.tsx";
@@ -19,8 +20,10 @@ const router = createBrowserRouter([
 ]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Header />
-    <Toaster />
-    <RouterProvider router={router} />
+    <GoogleOAuthProvider clientId="1075418359786-c91d8abaaaspc4dmkta33uo4chjcgbuo.apps.googleusercontent.com">
+      <Header />
+      <Toaster />
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
