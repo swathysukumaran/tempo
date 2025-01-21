162b386ca12881305b5cc223e8b312b4038c84a2
swathysukumaran <swathysukumaran@gmail.com>
2025-01-20 18:58:46 -0800

    Add sign in dialog

--- tempo/src/create-trip/index.tsx
+++ tempo/src/create-trip/index.tsx
@@ -9,6 +9,15 @@ import {
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { chatSession } from "@/service/AIModal";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 type Option = {
   label: string;
   value: string;
@@ -19,6 +28,7 @@ type FormData = {
 function CreateTrip() {
   const [place, setPlace] = useState<Option | null>(null);
   const [formData, setFormData] = useState<FormData>({});
+  const [openDialog, setOpenDialog] = useState(false);
   const handleInputChange = (
     name: string,
     value: string | number | boolean
@@ -35,6 +45,7 @@ function CreateTrip() {
   const onGenerateTrip = async () => {
     const user = localStorage.getItem("user");
     if (!user) {
+      setOpenDialog(true);
       return;
     }
     if (
@@ -150,6 +161,16 @@ function CreateTrip() {
       <div className="my-10 justify-end flex">
         <Button onClick={onGenerateTrip}>Generate Trip</Button>
       </div>
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
