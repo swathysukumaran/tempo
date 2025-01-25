c5ce30a564364cfed619cb72fc250b371f5ff8ec
swathysukumaran <swathysukumaran@gmail.com>
2025-01-24 23:06:06 -0800

    Setup mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import bodyparser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app=express();
 
@@ -20,3 +21,9 @@ const server=http.createServer(app);
 server.listen(8081,()=>{
     console.log('Server is running on http://localhost:8081/');
 });
+
+const MONGO_URL="mongodb+srv://swathysukumaran:g4pGjXwj22IlponE@cluster0.95gfw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise=Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error',(error:Error)=>{console.log(error);});
