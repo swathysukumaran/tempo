5b5a4d48b60402e73b227efded291656ee3ba791
swathysukumaran <swathysukumaran@gmail.com>
2025-01-25 12:30:13 -0800

    Fix update error

--- backend/src/db/users.ts
+++ backend/src/db/users.ts
@@ -19,4 +19,4 @@ export const getUserBySessionToken=(sessionToken:string)=>UserModel.findOne({'au
 export const getUserById=(id:string)=>UserModel.findById(id);
 export const createUser=(values:Record<string,any>)=>new UserModel(values).save().then((user)=>user.toObject());
 export const deleteUserById=(id:string)=>UserModel.findByIdAndDelete({_id:id});
-export const updateUserById=(id:string,values:Record<string,any>)=>UserModel.findByIdAndUpdate(id,values:Record<string,any>)=>UserModel.findByIdAndUpdate(id,values);
+export const updateUserById=(id:string,values:Record<string,any>)=>UserModel.findByIdAndUpdate(id,values);
