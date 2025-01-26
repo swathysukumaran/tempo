93219bfefafb06f642ad09a8f5302c37bae0dffa
swathysukumaran <swathysukumaran@gmail.com>
2025-01-26 10:29:53 -0800

    Integrate gemini api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import router from './router';
-
+require('dotenv').config();
 const app=express();
 
 app.use(cors({
