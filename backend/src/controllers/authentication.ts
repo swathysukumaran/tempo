686b457044298650014058646d34988ff913fc3b
swathysukumaran <swathysukumaran@gmail.com>
2025-01-25 16:43:08 -0800

    Create login controller

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -36,3 +36,34 @@ export const register: express.RequestHandler = async(req:express.Request,res:ex
         return;
     }
 }
+export const login=async (req:express.Request,res:express.Response)=>{
+    try{
+        const {email,password}=req.body;
+        if(!email || !password){
+            res.status(400);
+            return;
+        }
+        const user=await getUserByEmail(email).select('+authentication.salt +authentication.password');
+        if(!user){
+            res.status(400);
+            return;
+        }
+        const expectedHash=authentication(user.authentication.salt,password);
+        if(expectedHash!=user.authentication.password){
+            res.status(403);
+            return;
+        }
+
+        const salt=random();
+        user.authentication.sessionToken=authentication(salt,user._id.toString());
+        await user.save();
+
+        res.cookie('TEMPO-AUTH',user.authentication.sessionToken,{domain:'localhost',path:'/'});
+        res.status(200).json(user);
+        return;
+    }catch(error){
+        console.error('Login Error:', error);
+        res.status(400);
+        return;
+    }
+}
--- backend/src/router/authentication.ts
+++ backend/src/router/authentication.ts
@@ -1,37 +1,8 @@
 import express from 'express';
 
-import { register } from '../controllers/authentication';
-import { getUserByEmail } from 'db/users';
-import { authentication, random } from 'helpers';
-
-export const login=async (req:express.Request,res:express.Response)=>{
-    try{
-        const {email,password}=req.body;
-        if(!email || !password){
-            res.status(400);
-            return;
-        }
-        const user=await getUserByEmail(email).select('+authentication.salt +authentication.password');
-        if(!user){
-            res.status(400);
-            return;
-        }
-        const expectedHash=authentication(user.authentication.salt,password);
-        if(expectedHash!=user.authentication.password){
-            res.status(403);
-            return;
-        }
-
-        const salt=random();
-        user.authentication.sessionToken=authentication(salt,user._id.toString());
-        await user.save();
-    }catch(error){
-        console.error('Login Error:', error);
-        res.status(400);
-        return;
-    }
-}
+import { register,login } from '../controllers/authentication';
 
 export default (router:express.Router)=>{
     router.post('/auth/register',register);
+    router.post('/auth/login',login);
 };
