fb2f24f84fcbd024df7ba5a9edd8a17cba01dd84
swathysukumaran <swathysukumaran@gmail.com>
2025-01-26 07:06:23 -0800

    Create isOwner middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -2,6 +2,29 @@ import express from 'express';
 import {get,identity,merge} from 'lodash';
 import {getUserBySessionToken} from '../db/users';
 
+export const isOwner=async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
+
+    try{
+        const {id}=req.params;
+        const currentUserId=get(req,'identity._id') as string;
+        if(!currentUserId){
+            res.status(403);
+            return;
+        }
+        if(currentUserId.toString() !== id){
+            res.status(403);
+            return;
+        }
+        next();
+
+    }catch(error){
+        console.error('Authorization Error:',error);
+        res.status(400);
+        return
+
+    }
+}
+
 export const isAuthenticated=async(req:express.Request,res:express.Response,next:express.NextFunction)=>{
 
     try{
--- backend/src/router/users.ts
+++ backend/src/router/users.ts
@@ -1,10 +1,11 @@
 
 import { deleteUser } from 'controllers/users';
 import express from 'express';
+import { isAuthenticated, isOwner } from 'middlewares';
 
 export default (router:express.Router)=>{
    
-    router.delete('/users/:id',deleteUser);
- 
+    router.delete('/users/:id',isOwner,deleteUser);
+    
 }
 
