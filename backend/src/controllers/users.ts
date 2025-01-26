94553b01cebd0fee8c049f6f7c365a5c0e236593
swathysukumaran <swathysukumaran@gmail.com>
2025-01-26 07:49:48 -0800

    Create update controller

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,5 +1,5 @@
 
-import { deleteUserById } from 'db/users';
+import { deleteUserById, getUserById } from 'db/users';
 import express from 'express';
 
 export const deleteUser=async(req:express.Request,res:express.Response)=>{
@@ -7,6 +7,27 @@ export const deleteUser=async(req:express.Request,res:express.Response)=>{
         const {id}=req.params;
         const deletedUser=await deleteUserById(id);
         res.json(deletedUser);
+        return;
+
+    }catch(error){
+        console.log(error);
+        res.sendStatus(400);
+        return;
+    }
+}
+
+export const updateUser =async (req:express.Request,res:express.Response)=>{
+    try{
+        const {id}=req.params;
+        const {username}=req.body;
+        if(!username){
+            res.status(400);
+            return;
+        }
+        const user=await getUserById(id);
+        user.username=username;
+        await user.save();
+        res.sendStatus(200).json(user).end();
         return;
 
     }catch(error){
