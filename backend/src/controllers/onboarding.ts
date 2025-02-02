1d09533dbe4883f39b855800b60dbd5e29c0b2f2
swathysukumaran <swathysukumaran@gmail.com>
2025-02-02 13:33:54 -0800

    Complete onboarding controller

--- backend/src/controllers/onboarding.ts
+++ backend/src/controllers/onboarding.ts
@@ -1,4 +1,4 @@
-import { startOnboarding } from '../db/onboarding';
+import { getOnboarding, startOnboarding, updateOnboarding } from '../db/onboarding';
 import express, { response } from 'express';
 import { get } from 'lodash';
 
@@ -13,6 +13,40 @@ export const createOnboarding = async (req: express.Request, res: express.Respon
         res.status(200).json({onboarding});
         return;
 
+    }catch(error){
+        res.sendStatus(500);
+        return;
+    }
+}
+
+export const updateOnboardingSteps=async(req:express.Request,res:express.Response)=>{
+    try{
+        const userId=get(req,'identity._id');
+        if(!userId){
+            return res.sendStatus(403);
+        }
+        const {status,completedSteps,preferences}=req.body;
+        const onboarding=await updateOnboarding(userId,status,completedSteps,preferences);
+        res.status(200).json({onboarding});
+        return;
+
+    }catch(error){
+        res.sendStatus(500);
+        return;
+    }
+}
+
+export const getOnboardingStatus=async(req:express.Request,res:express.Response)=>{
+
+    try{
+
+        const userId=get(req,'identity._id');
+        if(!userId){
+            return res.sendStatus(403);
+        }
+        const onboarding=await getOnboarding(userId);
+        res.status(200).json({onboarding});
+        return;
     }catch(error){
         res.sendStatus(500);
         return;
