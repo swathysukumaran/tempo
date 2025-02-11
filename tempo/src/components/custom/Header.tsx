e28e8d28729157a708857480f9da677f1680fc13
swathysukumaran <swathysukumaran@gmail.com>
2025-02-11 00:32:16 -0800

    Show header conditionally

--- tempo/src/components/custom/Header.tsx
+++ tempo/src/components/custom/Header.tsx
@@ -5,7 +5,11 @@ import logo from "../../assets/logo.png";
 function Header() {
   return (
     <div className="p-2 shadow-sm flex justify-between items-center ">
-      <img src={logo} alt="logo" className="w-auto h-12 sm:h-16 md:h-20" />
+      <img
+        src={logo}
+        alt="logo"
+        className="w-auto h-12 mx-auto sm:h-16 md:h-20"
+      />
       {/* <div>
         <Button>Create new trip</Button>
       </div> */}
--- tempo/src/main.tsx
+++ tempo/src/main.tsx
@@ -2,7 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useLocation,
+} from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import CreateTrip from "./create-trip/index.tsx";
 import Header from "./components/custom/Header.tsx";
@@ -13,41 +18,61 @@ import TripDetails from "./components/custom/tripDetails.tsx";
 import Onboarding from "./components/custom/Onboarding.tsx";
 import AuthLayout from "./components/custom/AuthLayout.tsx";
 
+// Create a layout component that conditionally renders Header
+const RootLayout = () => {
+  const location = useLocation();
+
+  // List of paths where Header should not be shown
+  const noHeaderPaths = ["/login", "/", "/register"];
+
+  return (
+    <>
+      {!noHeaderPaths.includes(location.pathname) && <Header />}
+      <Outlet />
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
-    element: <AuthLayout />,
+    element: <RootLayout />,
     children: [
       {
-        path: "/",
-        element: <Register />,
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "/",
+            element: <Register />,
+          },
+          {
+            path: "/login",
+            element: <Login />,
+          },
+        ],
       },
       {
-        path: "/login",
-        element: <Login />,
+        path: "/home",
+        element: <App />,
+      },
+      {
+        path: "/create-trip",
+        element: <CreateTrip />,
+      },
+      {
+        path: "/trip-details/:tripId",
+        element: <TripDetails />,
+      },
+      {
+        path: "/onboarding",
+        element: <Onboarding />,
       },
     ],
   },
-  {
-    path: "/home",
-    element: <App />,
-  },
-  {
-    path: "/create-trip",
-    element: <CreateTrip />,
-  },
-  {
-    path: "/trip-details/:tripId",
-    element: <TripDetails />,
-  },
-  {
-    path: "/onboarding",
-    element: <Onboarding />,
-  },
 ]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="1075418359786-c91d8abaaaspc4dmkta33uo4chjcgbuo.apps.googleusercontent.com">
-      <Header />
       <Toaster />
       <RouterProvider router={router} />
     </GoogleOAuthProvider>
