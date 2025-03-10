ec7cc3633a60953de572f53fd8b53f2949c305a2
swathysukumaran <swathysukumaran@gmail.com>
2025-03-10 15:05:28 -0700

    Add create trip button in header

--- tempo/src/components/custom/Header.tsx
+++ tempo/src/components/custom/Header.tsx
@@ -4,7 +4,7 @@ import logo from "../../assets/logo.png";
 import { Button } from "../ui/button";
 import { API_URL } from "@/config/api";
 import { useNavigate } from "react-router-dom";
-import { Globe } from "lucide-react";
+import { Globe, Compass } from "lucide-react";
 function Header() {
   const navigate = useNavigate();
   const logout = async () => {
@@ -32,10 +32,18 @@ function Header() {
       <img
         src={logo}
         alt="logo"
-        onClick={() => navigate("/")}
+        onClick={() => navigate("/create-trip-new")}
         className="w-auto h-12  sm:h-16 md:h-20 cursor-pointer"
       />
       <div className="flex items-center gap-4">
+        <Button
+          variant="ghost"
+          className="flex items-center gap-2"
+          onClick={() => navigate("/create-trip-new")}
+        >
+          <Compass size={20} />
+          Create Trip
+        </Button>
         <Button
           variant="ghost"
           className="flex items-center gap-2"
@@ -44,7 +52,11 @@ function Header() {
           <Globe size={20} />
           My Trips
         </Button>
-        <Button variant="ghost" onClick={handleLogout}>
+        <Button
+          variant="ghost"
+          className="bg-primary text-white"
+          onClick={handleLogout}
+        >
           Logout
         </Button>
       </div>
