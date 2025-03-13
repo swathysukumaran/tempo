e5330073b0ab9aad7b7e763e58a2a83504b2d896
swathysukumaran <swathysukumaran@gmail.com>
2025-03-13 16:07:45 -0700

    Create  Google geocoder object

--- tempo/src/components/custom/MapView.tsx
+++ tempo/src/components/custom/MapView.tsx
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   APIProvider,
   Map,
   AdvancedMarker,
   Pin,
   InfoWindow,
+  useMapsLibrary,
 } from "@vis.gl/react-google-maps";
 const apiKey = import.meta.env.VITE_GOOGLE_PLACE_API_KEY || "";
 const mapId = import.meta.env.VITE_GOOGLE_MAP_ID || "";
+const GeocodeLocations = ({ hotels, activities, onLocationsReady }) => {
+  const geocodingLibrary = useMapsLibrary("geocoding");
+  useEffect(() => {
+    if(!geocodingLibrary || !hotels || !activities) return;
+
+    const geocoder=new geocodingLibrary.Geocoder();
+    const allLocations = [];
+    const newBounds = new google.maps.LatLngBounds();
+    let pendingGeocodes=0;
+  }
+};
 function MapView() {
   const position = { lat: 53.54, lng: 10 };
   const [open, setOpen] = useState(false);
