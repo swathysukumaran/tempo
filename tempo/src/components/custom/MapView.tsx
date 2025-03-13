b2b263b8b7091cec7aae21c67dad59bdd78f9596
swathysukumaran <swathysukumaran@gmail.com>
2025-03-13 14:16:42 -0700

    Add basic google form component

---
+++ tempo/src/components/custom/MapView.tsx
@@ -0,0 +1,39 @@
+("use client");
+
+import { useState } from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+import { API_URL } from "@/config/api";
+function MapView() {
+  const position = { lat: 53.54, lng: 10 };
+  const [open, setOpen] = useState(false);
+
+  return (
+    <APIProvider apiKey={API_URL}>
+      <div style={{ height: "100vh", width: "100%" }}>
+        <Map zoom={9} center={position} mapId={process.env.NEXT_PUBLIC_MAP_ID}>
+          <AdvancedMarker position={position} onClick={() => setOpen(true)}>
+            <Pin
+              background={"grey"}
+              borderColor={"green"}
+              glyphColor={"purple"}
+            />
+          </AdvancedMarker>
+
+          {open && (
+            <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
+              <p>I'm in Hamburg</p>
+            </InfoWindow>
+          )}
+        </Map>
+      </div>
+    </APIProvider>
+  );
+}
+
+export default MapView;
