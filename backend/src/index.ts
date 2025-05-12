716a01cf622bd6c268c273dbf83e2dc51f32f074
swathysukumaran <swathysukumaran@gmail.com>
2025-05-12 14:43:57 -0700

    Move cookie parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,14 +8,14 @@ import mongoose from 'mongoose';
 import router from './router';
 require('dotenv').config();
 const app=express();
-
+app.use(cookieParser());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 
 app.use(compression());
-app.use(cookieParser());
+
 
 
 const server=http.createServer(app);
