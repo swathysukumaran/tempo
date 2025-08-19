9b64261dbe877ef0eb91b85a76503c4c8a623b8e
swathysukumaran <swathysukumaran@gmail.com>
2025-08-18 22:14:42 -0700

    Create extract function

--- backend/src/controllers/extractDataController.ts
+++ backend/src/controllers/extractDataController.ts
@@ -1,2 +1,18 @@
 import express from 'express';
 import {get} from 'lodash';
+const {
+  GoogleGenerativeAI,
+} = require("@google/generative-ai");
+require('dotenv').config({ path: '.env.local' });
+
+// Initialize the API
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || '');
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-exp' });
+
+// Extract data fields from the prompt
+export const extractDataFromPrompt= async (req:express.Request,res:express.Response)=>{
+    const {prompt}=req.body;
+    try{
+        
+    }
+}
