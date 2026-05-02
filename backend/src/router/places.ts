32bb6a71e8091e9b2119b1695d9ccd2f11560be2
swathysukumaran <swathysukumaran@gmail.com>
2026-05-02 12:31:33 -0700

    Protect lookupPlace api

--- backend/src/router/places.ts
+++ backend/src/router/places.ts
@@ -1,6 +1,7 @@
 import { lookupPlace } from '../controllers/places';
+import { isAuthenticated } from '../middlewares';
 import express from 'express';
 
 export default(router:express.Router)=>{
-    router.post('/lookup-place',lookupPlace);
+    router.post('/lookup-place', isAuthenticated, lookupPlace);
 }
