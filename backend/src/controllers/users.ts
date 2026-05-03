9e715a66bee7c6d62c72acdd83e39aba4948e758
swathysukumaran <swathysukumaran@gmail.com>
2026-05-02 19:02:17 -0700

    Add async handler to users

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,40 +1,24 @@
-
 import { deleteUserById, getUserById } from '../db/users';
 import express from 'express';
+import asyncHandler from '../helpers/asyncHandler';
 
-export const deleteUser = async (req: express.Request, res: express.Response) => {
-    try {
-        const { id } = req.params;
-        const deletedUser = await deleteUserById(id);
-        res.status(200).json(deletedUser);
-        return;
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to delete user' });
+export const deleteUser = asyncHandler(async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+    const deletedUser = await deleteUserById(id);
+    res.status(200).json(deletedUser);
+});
+
+export const updateUser = asyncHandler(async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    const user = await getUserById(id);
+    if (!user) {
+        res.status(404).json({ error: 'User not found' });
         return;
     }
-}
 
-export const updateUser = async (req: express.Request, res: express.Response) => {
-    try {
-        const { id } = req.params;
-        const { username } = req.body;
-        if (!username) {
-            res.status(400).json({ error: 'Username is required' });
-            return;
-        }
-        const user = await getUserById(id);
-        if (!user) {
-            res.status(404).json({ error: 'User not found' });
-            return;
-        }
-        user.username = username;
-        await user.save();
-        res.status(200).json(user);
-        return;
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to update user' });
-        return;
-    }
-}
+    user.username = username;
+    await user.save();
+    res.status(200).json(user);
+});
